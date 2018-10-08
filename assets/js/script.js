6c609df4a903ac14e808bdf0a1f29a0aeccd9920
Stiouf <stephane.quentin@live.fr>
2018-10-08 11:38:59 +0200

    correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 function ex6(){
   var first = document.getElementById("firstNumber").value; // récupération de la première variable que l'on a tapé dans la page html
   var second = document.getElementById("secondNumber").value;
-  alert("Reste : " + (first % second)); //Affcihage du reste de la division entre la première variable et la seconde
+
+  if(isNaN(first) || isNaN(second)){ // OU logique
+    alert('Erreur!');
+    } else {
+      alert("Reste de la division : " + (first % second)); // % renvoi le reste de la division entre les variables first et second
+}
 }
